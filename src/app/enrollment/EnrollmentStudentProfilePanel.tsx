0ba7ae1fbd2731d200feb2b67e3c604be9416d7c
Pool Golez <loop.edward@gmail.com>
2026-01-15 09:53:57 +0800

    Refactor enrollment student profile

--- src/app/enrollment/EnrollmentStudentProfilePanel.tsx
+++ src/app/enrollment/EnrollmentStudentProfilePanel.tsx
@@ -5,7 +5,6 @@ import {
   Typography,
   Box,
   TextField,
-  TextFieldProps,
   Stack,
   Divider,
   FormControl,
@@ -13,12 +12,9 @@ import {
   FormLabel,
   Radio,
   RadioGroup,
+  Button,
 } from "@mui/material";
-import { FC, useState } from "react";
-
-const StandardTextField: FC<TextFieldProps> = (props) => {
-  return <TextField variant="standard" {...props} />;
-};
+import { useState } from "react";
 
 type MODE = "existing" | "new";
 
@@ -34,8 +30,8 @@ export default function EnrollmentStudentProfilePanel() {
       <Typography variant="h6">Student Profile</Typography>
       <Paper elevation={2} sx={{ p: 4 }}>
         <Stack spacing={4}>
-          <Stack direction="row" spacing={2}>
-            <Box flex={1}>
+          <Stack direction="row" spacing={2} alignItems="center">
+            <Box>
               <FormControl>
                 <FormLabel>Existing/New Student</FormLabel>
                 <RadioGroup row value={mode} onChange={handleChange}>
@@ -52,95 +48,103 @@ export default function EnrollmentStudentProfilePanel() {
                 </RadioGroup>
               </FormControl>
             </Box>
-            <Box flex={1}>
-              {mode === "existing" && <StandardTextField label="Student ID" />}
-            </Box>
+            {mode === "existing" && (
+              <Stack direction="row" alignItems="baseline" spacing={2}>
+                <TextField variant="standard" label="Student ID" />
+                <Button variant="contained" size="small">
+                  Search
+                </Button>
+              </Stack>
+            )}
           </Stack>
         </Stack>
 
         {mode == "new" && (
-          <Stack spacing={4}>
-            <Box>
-              <Divider
-                textAlign="left"
-                sx={{ borderStyle: "dashed", borderColor: "divider" }}
-              >
-                <Typography variant="subtitle2" color="primary.light">
+          <Stack spacing={2}>
+            <Divider
+              textAlign="left"
+              sx={{ borderStyle: "dashed", borderColor: "divider" }}
+            >
+              <Typography variant="subtitle2" color="primary.main"></Typography>
+            </Divider>
+
+            <Stack component="form" spacing={2}>
+              <Box>
+                <Typography variant="subtitle2" color="primary.main">
                   Student Information
                 </Typography>
-              </Divider>
-
-              <Stack component="form" spacing={2}>
-                <Stack direction="row" spacing={2}>
-                  <Box flex={1}>
-                    <StandardTextField fullWidth label="First Name" />
-                  </Box>
-                  <Box flex={1}>
-                    <StandardTextField label="Middle Name" />
-                  </Box>
-                  <Box flex={1}>
-                    <StandardTextField label="Family Name" />
-                  </Box>
-                  <Box flex={1}>
-                    <StandardTextField label="Suffix" />
-                  </Box>
-                  <Box flex={2} />
-                </Stack>
-                <Stack direction="row" spacing={2}>
-                  <Box flex={1}>
-                    <StandardTextField label="Gender" />
-                  </Box>
-                  <Box flex={1}>
-                    <StandardTextField label="Date of Birth" />
-                  </Box>
-                  <Box flex={1}>
-                    <StandardTextField label="Nationality" />
-                  </Box>
-                  <Box flex={3} />
-                </Stack>
-                <Stack direction="row" spacing={2}>
-                  <Box flex={4}>
-                    <StandardTextField fullWidth label="Address" />
-                  </Box>
-                  <Box flex={2} />
-                </Stack>
-              </Stack>
-            </Box>
-
-            <Box>
-              <Divider
-                textAlign="left"
-                sx={{ borderStyle: "dashed", borderColor: "divider" }}
-              />
-
-              <Stack component="form" spacing={2}>
-                <Box>
-                  <Typography variant="subtitle2" color="primary.light">
-                    Parent/Guardian Information
-                  </Typography>
+              </Box>
+              <Stack direction="row" spacing={2}>
+                <Box flex={1}>
+                  <TextField variant="standard" fullWidth label="First Name" />
                 </Box>
-                <Stack direction="row" spacing={2}>
-                  <Box flex={3}>
-                    <StandardTextField fullWidth label="Name" />
-                  </Box>
-                  <Box flex={1}>
-                    <StandardTextField label="Relationship" />
-                  </Box>
-                  <Box flex={2} />
-                </Stack>
-                <Stack direction="row" spacing={2}>
-                  <Box flex={1}>
-                    <StandardTextField label="Contact Number" />
-                  </Box>
-                  <Box flex={1}>
-                    <StandardTextField fullWidth label="Email Address" />
-                  </Box>
-                  <Box flex={4}>
-                    <StandardTextField fullWidth label="Address" />
-                  </Box>
-                </Stack>
+                <Box flex={1}>
+                  <TextField variant="standard" label="Middle Name" />
+                </Box>
+                <Box flex={1}>
+                  <TextField variant="standard" label="Family Name" />
+                </Box>
+                <Box flex={1}>
+                  <TextField variant="standard" label="Suffix" />
+                </Box>
+                <Box flex={2} />
               </Stack>
-            </Box>
+              <Stack direction="row" spacing={2}>
+                <Box flex={1}>
+                  <TextField variant="standard" label="Gender" />
+                </Box>
+                <Box flex={1}>
+                  <TextField variant="standard" label="Date of Birth" />
+                </Box>
+                <Box flex={1}>
+                  <TextField variant="standard" label="Nationality" />
+                </Box>
+                <Box flex={3} />
+              </Stack>
+              <Stack direction="row" spacing={2}>
+                <Box flex={4}>
+                  <TextField variant="standard" fullWidth label="Address" />
+                </Box>
+                <Box flex={2} />
+              </Stack>
+            </Stack>
+
+            <Divider
+              textAlign="left"
+              sx={{ borderStyle: "dashed", borderColor: "divider" }}
+            />
+
+            <Stack component="form" spacing={2}>
+              <Box>
+                <Typography variant="subtitle2" color="primary.main">
+                  Parent/Guardian Information
+                </Typography>
+              </Box>
+              <Stack direction="row" spacing={2}>
+                <Box flex={3}>
+                  <TextField variant="standard" fullWidth label="Name" />
+                </Box>
+                <Box flex={1}>
+                  <TextField variant="standard" label="Relationship" />
+                </Box>
+                <Box flex={2} />
+              </Stack>
+              <Stack direction="row" spacing={2}>
+                <Box flex={1}>
+                  <TextField variant="standard" label="Contact Number" />
+                </Box>
+                <Box flex={1}>
+                  <TextField
+                    variant="standard"
+                    fullWidth
+                    label="Email Address"
+                  />
+                </Box>
+                <Box flex={4}>
+                  <TextField variant="standard" fullWidth label="Address" />
+                </Box>
+              </Stack>
+            </Stack>
           </Stack>
         )}
       </Paper>
